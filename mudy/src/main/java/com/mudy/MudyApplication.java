787a2a68eebccfe1fcb8e8acabb0dc6206f6451c
Ivan-jauregui <162524712+Ivan-jauregui@users.noreply.github.com>
2026-06-14 13:54:49 -0300

    Add files via upload

---
+++ mudy/src/main/java/com/mudy/MudyApplication.java
@@ -0,0 +1,13 @@
+package com.mudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudyApplication.class, args);
+	}
+
+}
---
+++ mudy/src/test/java/com/mudy/MudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
